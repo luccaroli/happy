ff90f75cbc21ebd717fbb624dfed2943113e495a
luccaroli <luccaroli20@gmail.com>
2020-10-13 20:01:43 -0300

    fist commit server of app

---
+++ server/src/views/orphanage_view.ts
@@ -0,0 +1,21 @@
+import Orphanage from '../models/Orphanage'
+import imagesView from './image_view'
+
+export default {
+  render(orphanage: Orphanage) {
+    return {
+      id: orphanage.id,
+      name: orphanage.name,
+      latitude: orphanage.latitude,
+      longitude: orphanage.longitude,
+      about: orphanage.about,
+      instructions: orphanage.instructions,
+      opening_hours: orphanage.opening_hours,
+      open_on_weekends: orphanage.open_on_weekends,
+      images: imagesView.renderMany(orphanage.images)
+    }
+  },
+  renderMany(orphanage: Orphanage[]) {
+    return orphanage.map(orphanage => this.render(orphanage))
+  }
+}
